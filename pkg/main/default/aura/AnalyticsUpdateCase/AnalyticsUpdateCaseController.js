3efea1f8f86be14bda95443d0a7ecd2bc1f095cb
Dominic Ley <aautcsh@lain.local>
2017-10-19 19:42:10 +0200

    Initial commit

---
+++ pkg/main/default/aura/AnalyticsUpdateCase/AnalyticsUpdateCaseController.js
@@ -0,0 +1,26 @@
+({
+    "queryAmount" : function(cmp) {
+        var action = cmp.get("c.queryAmountAccount");
+        action.setParams({caseId : cmp.get("v.recordId")});
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                alert("Update initialized");
+            }
+
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+
+        $A.enqueueAction(action);
+    }
+})
