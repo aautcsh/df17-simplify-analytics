ce87b90b9489f5841a45318e105c8c05b9daaf82
Dominic L <aautcsh@gmail.com>
2017-10-22 20:21:23 +0200

    Fixed indentation for AnalyticsUpdateCaseController.js

--- pkg/main/default/aura/AnalyticsUpdateCase/AnalyticsUpdateCaseController.js
+++ pkg/main/default/aura/AnalyticsUpdateCase/AnalyticsUpdateCaseController.js
@@ -1,26 +1,29 @@
 ({
-    "queryAmount" : function(cmp) {
-        var action = cmp.get("c.queryAmountAccount");
-        action.setParams({caseId : cmp.get("v.recordId")});
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                alert("Update initialized");
-            }
-
-            else if (state === "ERROR") {
-                var errors = response.getError();
-                if (errors) {
-                    if (errors[0] && errors[0].message) {
-                        console.log("Error message: " + 
-                                 errors[0].message);
-                    }
-                } else {
-                    console.log("Unknown error");
-                }
-            }
-        });
-
-        $A.enqueueAction(action);
-    }
+  "queryAmount" : function(cmp) {
+    var action = cmp.get("c.queryAmountAccount");
+    
+    action.setParams({
+      caseId : cmp.get("v.recordId")
+    });
+    
+    action.setCallback(this, function(response) {
+      var state = response.getState();
+      if (state === "SUCCESS") {
+        alert("Update initialized");
+      }
+      else if (state === "ERROR") {
+        var errors = response.getError();
+        if (errors) {
+          if (errors[0] && errors[0].message) {
+            console.log("Error message: " + 
+              errors[0].message);
+          }
+        } 
+        else {
+          console.log("Unknown error");
+        }
+      }
+    });
+    $A.enqueueAction(action);
+  }
 })
